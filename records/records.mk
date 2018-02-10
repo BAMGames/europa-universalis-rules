c35471bd003579d5fc419b2ab2bf9e4cb82e135f
Jean-Yves Moyen <Jean-Yves.Moyen@ens-lyon.org>
2018-02-10 16:21:33 +0100

    Create first trun RT sheets for each country.

--- records/records.mk
+++ records/records.mk
@@ -1,11 +1,12 @@
 # Local defs
 RECORDSCOUNTRY = ANG DAN FRA HIS POL POR RUS TUR VEN
 RECORDSCOUNTRYINCSTEM=$(addprefix accounting-income-,$(RECORDSCOUNTRY))
+RECORDSCOUNTRYRTSTEM=$(addprefix accounting-rt-,$(RECORDSCOUNTRY))
 
 RECORDSGSEXTRA = -SCURRENTDIR=$(RECORDSDIR)
 
 RECORDSCOLSTEMS=spanishcolonies colonies monarch
-RECORDSACCSTEMS=accounting-rt accounting-income accounting-rt-start accounting-income-start $(RECORDSCOUNTRYINCSTEM)
+RECORDSACCSTEMS=accounting-rt accounting-income accounting-rt-start accounting-income-start $(RECORDSCOUNTRYINCSTEM) $(RECORDSCOUNTRYRTSTEM)
 RECORDSTEXSTEMS=exoticresources tradefleets
 
 COLRECORDSFILES=$(addprefix $(RECORDSDIR)/,$(addsuffix .pdf,$(RECORDSCOLSTEMS)))
