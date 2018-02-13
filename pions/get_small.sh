a516b7093ddd2dbc64faff1c57ecc1343771e61d
Jean-Yves Moyen <Jean-Yves.Moyen@ens-lyon.org>
2018-02-13 08:25:29 +0100

    Correct some paper size.

--- pions/get_small.sh
+++ pions/get_small.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-pdfjam pions.pdf 1,11,16,19-21,26 -o small_single.pdf
+pdfjam --paper letter pions.pdf 1,11,16,19-21,26 -o small_single.pdf
 
-pdfjam pions.pdf 2-3,14-15,17-18,24-25 -o small_double.pdf
+pdfjam --paper letter pions.pdf 2-3,14-15,17-18,24-25 -o small_double.pdf
