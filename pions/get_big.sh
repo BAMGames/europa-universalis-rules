6c527cabb4a46f9b615c919f2410821650ee8b23
Jym <Jean-Yves.Moyen@ens-lyon.org>
2018-02-13 21:07:40 +0100

    Adjust counters scripts.

--- pions/get_big.sh
+++ pions/get_big.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-pdfjam pions.pdf 4,36 -o big_single.pdf
+pdfjam --papersize '{5.5in,8.5in}' pions.pdf 4,36 -o big_single.pdf
 
-pdfjam pions.pdf 5-12,14-15,27-30,37-40 -o big_double.pdf
+pdfjam --papersize '{5.5in,8.5in}' pions.pdf 5-12,14-15,27-30,37-40 -o big_double.pdf
--- pions/get_small.sh
+++ pions/get_small.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-pdfjam --paper letter pions.pdf 1,11,16,19-21,26 -o small_single.pdf
+pdfjam --paper letter pions.pdf 1,11,16,19-20,23-24 -o small_single.pdf
 
-pdfjam --paper letter pions.pdf 2-3,14-15,17-18,24-25 -o small_double.pdf
+pdfjam --paper letter pions.pdf 2-3,14-15,17-18,27-28 -o small_double.pdf
