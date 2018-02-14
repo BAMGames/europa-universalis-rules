0491dd4bd6db3520ac9b0495d5eccdc426732679
Jym <Jean-Yves.Moyen@ens-lyon.org>
2018-02-14 08:02:00 +0100

    .gitignore and cleaning

--- pions/get_big.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-pdfjam --papersize '{5.5in,8.5in}' pions.pdf 4,36 -o big_single.pdf
-
-pdfjam --papersize '{5.5in,8.5in}' pions.pdf 5-12,14-15,27-30,37-40 -o big_double.pdf
--- pions/get_small.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-pdfjam --paper letter pions.pdf 1,11,16,19-20,23-24 -o small_single.pdf
-
-pdfjam --paper letter pions.pdf 2-3,14-15,17-18,27-28 -o small_double.pdf
